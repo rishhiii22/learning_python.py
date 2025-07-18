98cf999efcc856cbfa166e51ccaa482c147a6e4b
Rishi Dadhich <rishidadhich22@gmail.com>
2025-07-18 22:14:10 +0530

    Add files via upload

---
+++ module.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+print("Hello World")
+# Fetch a random joke   ( this is single line comment )
+joke= pyjokes.get_joke()
+print(joke)
+
+"""
+Hello My Name is Rishi
+and I am a Python Developer
+""" (this is multi-line comment )
